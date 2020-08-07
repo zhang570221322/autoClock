db915171f0a78a57da0fd63e6ab93b07e7b24015
zhang570221322 <570221322@qq.com>
2020-08-07 19:11:55 +0800

    add log

--- autoDiDi.js
+++ autoDiDi.js
@@ -3,13 +3,18 @@ const schedule = require('node-schedule'); // 定时任务
 const YAML = require('yamljs'); //读取配置文件
 const fs = require("fs"); // 解析
 yaml_file="main.yml" //配置文件路径
+const moment = require('moment');// 时间
 const data = YAML.parse(fs.readFileSync(yaml_file).toString());
 
 
 const  scheduleCronstyle = ()=>{
-
+   
     schedule.scheduleJob( data['config']['scheduleJob'],()=>{
+        log=moment(Date.now()).format('YYYY-MM-DD-HH-mm-ss')+": 开始批量打卡"
+        console.log(log);
         my(true)
+        log=moment(Date.now()).format('YYYY-MM-DD-HH-mm-ss')+": 结束批量打卡"
+        console.log(log);
     }); 
 }
 
